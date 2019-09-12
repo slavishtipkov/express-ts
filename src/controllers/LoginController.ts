cbae1ea20093e203b6bf085fbc453890a7e24fb2
Slavi Shtipkov <slavishtipkov1993@gmail.com>
2019-09-12 23:09:27 +0300

    small app

---
+++ src/controllers/LoginController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express';
+import { get, controller, bodyValidator, post } from './decorators';
+
+@controller('/auth')
+class LoginController {
+  @get('/login')
+  getLogin(req: Request, res: Response): void {
+    res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input name="password" type="password" />
+      </div>
+      <button>Submit</buttom>
+    </form>
+  `);
+  }
+
+  @post('/login')
+  @bodyValidator('email', 'password')
+  postLogin(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    if (email && password && email === 'hi@hi.com' && password === 'pass') {
+      req.session = { loggedIn: true };
+      res.redirect('/');
+
+      res.send({ email, password });
+    } else {
+      res.send('Invalid email or password!');
+    }
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { router } from './routes/loginRoutes';
+import { AppRouter } from './AppRouter';
+
+import './controllers/LoginController';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['laskdjf'] }));
+
+app.use(router);
+app.use(AppRouter.getInstance());
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
